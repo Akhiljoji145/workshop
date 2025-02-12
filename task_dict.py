c76a8875a6f072253f27d185f745bf8442ac0770
Akhiljoji145 <akhiljoji1451@gmail.com>
2025-02-12 12:33:22 +0530

    modified

--- task_dict.py
+++ task_dict.py
@@ -1,4 +1,9 @@
-student = {{'studentname': 'neethu', 'mark': 25},{'studentname': 'anu', 'mark': 30},{'studentname': 'akhil', 'mark': 20}, {'studentname': 'akash', 'mark': 40},{'studentname': 'adarsh', 'mark': 50}}
-name=input("enter student name : ")
-for i in student['studentname']:
-    print(i)
+student = [{'studentname': 'neethu', 'mark': 25},{'studentname': 'anu', 'mark': 30},{'studentname': 'akhil', 'mark': 20}, {'studentname': 'akash', 'mark': 40},{'studentname': 'adarsh', 'mark': 50}]
+name = input("Enter student name: ")
+
+for i in student:
+    if i['studentname'] == name:
+        print(i['mark'])
+        break
+else:
+    print("name not found")
